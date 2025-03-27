93950a6b5a58f93bf080703f05c34cba2098351e
octvs <octvs@posteo.de>
2025-03-27 16:17:01 +0100

    wip

--- pkgs/development/python-modules/selectolax/default.nix
+++ pkgs/development/python-modules/selectolax/default.nix
@@ -1,30 +1,48 @@
 {
   lib,
   buildPythonPackage,
-  fetchPypi,
-
+  fetchFromGitHub,
   # build-system
   setuptools,
+  cython,
+  # test
+  pytestCheckHook,
 }:
 buildPythonPackage rec {
   pname = "selectolax";
   version = "0.3.28";
   pyproject = true;
 
-  src = fetchPypi {
-    inherit pname version;
-    hash = "sha256-iuPalyYbV77VH6mwPEiJhfbc/y4uREcaqfXiwXzBxFo=";
+  src = fetchFromGitHub {
+    owner = "rushter";
+    repo = "selectolax";
+    tag = "v${version}";
+    hash = "sha256-41KVec4LPWJitmJgmv4D2DHwanZl4LVoFk//rUv2rMU=";
+    fetchSubmodules = true;
+    leaveDotGit = true;
+    postFetch = ''
+      cd $src
+      touch anan.xml
+      # Download connectedhomeip.
+      git fetch
+      git reset --hard HEAD
+      git submodule update --init --depth 1 lexbor modest
+    '';
   };
 
-  build-system = [ setuptools ];
+  build-system = [setuptools cython];
+  #
+  # nativeCheckInputs = [pytestCheckHook];
+  #
+  # pythonImportsCheck = ["selectolax"];
 
-  pythonImportsCheck = [ "selectolax" ];
+  doCheck = false;
 
   meta = {
     description = "Python binding to Modest and Lexbor engines (fast HTML5 parser with CSS selectors).";
     homepage = "https://github.com/rushter/selectolax";
     changelog = "https://github.com/rushter/selectolax/releases/tag/${version}";
     license = lib.licenses.mit;
-    maintainers = [ lib.maintainers.octvs ];
+    maintainers = [lib.maintainers.octvs];
   };
 }
